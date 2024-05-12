66e7da55f3a25047977e173e77b42383736235b9
joshua <ayiniiayi@gmail.com>
2024-05-12 18:33:54 +0100

    testing

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="">
       <div className="hero_main">
-        <div> Testing </div>
+        <div> testing</div>
         
         
       </div>
