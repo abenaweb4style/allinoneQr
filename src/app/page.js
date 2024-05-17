e691318ea5967dd1413436d2ce82de1d170576da
joshua <ayiniiayi@gmail.com>
2024-05-17 16:51:46 +0100

    added locations to site

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,7 @@ import backgroundImage from "./images/44.jpg"
 import backgroundImage2 from "./images/17.jpg"
 import backgroundImage3 from "./images/44.jpg"
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 
 
@@ -16,7 +17,7 @@ export default function Home() {
   const [num, setNum] = useState(0)
 
   const ImageRotator = () => {
-    
+
 
     useEffect(() => {
       const intervalId = setInterval(() => {
@@ -24,7 +25,7 @@ export default function Home() {
       }, 4000); // Change image every 500ms
       return () => clearInterval(intervalId); // Clean up on unmount
     }, [img.length])
-   
+
   }
 
   ImageRotator()
@@ -45,6 +46,7 @@ export default function Home() {
         <div className="container">
           <div>testing</div>
         </div>
+        
 
 
       </div>
@@ -66,6 +68,47 @@ export default function Home() {
 
       </div>
       <Socials />
+
+      <div className='map-wrapper'>
+
+
+        <div className='google-map'>
+          <iframe
+           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3970.8212785584456!2d-0.1833521261269684!3d5.59340839438738!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf9afa0e163461%3A0xf035cab6e6449125!2sLancaster%20Accra!5e0!3m2!1sen!2sgh!4v1715960113674!5m2!1sen!2sgh"
+            style={{ border: '0' }} allowfullscreen=""
+            loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <div className='map-text'>
+            <h3>Lancaster Hotel, African Store</h3>
+            <p>Lancaster </p>
+            <Link target='_blank' href='https://www.google.com/maps/place/Nagla+Cl,+Accra/@5.592356,-0.256042,16z/data=!4m6!3m5!1s0xfdf99b1ea6b313d:0xca8f465a57b43f0a!8m2!3d5.5923556!4d-0.256042!16s%2Fg%2F11f5w8xg_l?hl=en&entry=ttu'>Get Directions</Link>
+
+          </div>
+        </div>
+        <div className='google-map'>
+          <iframe
+           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3970.8212785584456!2d-0.1833521261269684!3d5.59340839438738!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf9afa0e163461%3A0xf035cab6e6449125!2sLancaster%20Accra!5e0!3m2!1sen!2sgh!4v1715960113674!5m2!1sen!2sgh"
+            style={{ border: '0' }} allowfullscreen=""
+            loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <div className='map-text'>
+            <h3>Lancaster Hotel, African Store</h3>
+            <p>Lancaster </p>
+            <Link target='_blank' href='https://www.google.com/maps/place/Nagla+Cl,+Accra/@5.592356,-0.256042,16z/data=!4m6!3m5!1s0xfdf99b1ea6b313d:0xca8f465a57b43f0a!8m2!3d5.5923556!4d-0.256042!16s%2Fg%2F11f5w8xg_l?hl=en&entry=ttu'>Get Directions</Link>
+
+          </div>
+        </div>
+        <div className='google-map'>
+          <iframe
+           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3970.8212785584456!2d-0.1833521261269684!3d5.59340839438738!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf9afa0e163461%3A0xf035cab6e6449125!2sLancaster%20Accra!5e0!3m2!1sen!2sgh!4v1715960113674!5m2!1sen!2sgh"
+            style={{ border: '0' }} allowfullscreen=""
+            loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <div className='map-text'>
+            <h3>Lancaster Hotel, African Store</h3>
+            <p>Lancaster </p>
+            <Link target='_blank' href='https://www.google.com/maps/place/Nagla+Cl,+Accra/@5.592356,-0.256042,16z/data=!4m6!3m5!1s0xfdf99b1ea6b313d:0xca8f465a57b43f0a!8m2!3d5.5923556!4d-0.256042!16s%2Fg%2F11f5w8xg_l?hl=en&entry=ttu'>Get Directions</Link>
+
+          </div>
+        </div>
+      </div>
       <div className="spacer"></div>
       {/* <Hero/> */}
     </main>
